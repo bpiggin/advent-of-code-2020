219f908f934343d54643527de9718696d3f66096
Benjamin Piggin <piggin.ben@gmail.com>
2020-12-04 23:26:16 +0000

    use switch

--- src/day4/day4.part2.ts
+++ src/day4/day4.part2.ts
@@ -2,37 +2,33 @@ const required = ['byr', 'iyr', 'eyr', 'hgt', 'hcl', 'ecl', 'pid'];
 
 const isEntryValid = (entry: string) => {
   const [field, value] = entry.split(':');
-  if (field === 'byr') {
-    return 1920 <= +value && +value <= 2002;
+  switch (field) {
+    case 'byr':
+      return 1920 <= +value && +value <= 2002;
+    case 'iyr':
+      return 2010 <= +value && +value <= 2020;
+    case 'eyr':
+      return 2020 <= +value && +value <= 2030;
+    case 'hgt':
+      const match = value.match(new RegExp(/^[\d]+(cm|in)$/));
+      if (!match) {
+        return false;
+      }
+      const unit = value.slice(value.length - 2);
+      const number = value.slice(0, value.length - 2);
+      if (unit === 'cm') {
+        return 150 <= +number && +number <= 193;
+      }
+      return 59 <= +number && +number <= 76;
+    case 'hcl':
+      return !!value.match(new RegExp(/^#[0-9a-f]{6}$/));
+    case 'ecl':
+      return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
+    case 'pid':
+      return !!value.match(new RegExp(/^\d{9}$/));
+    default:
+      return true;
   }
-  if (field === 'iyr') {
-    return 2010 <= +value && +value <= 2020;
-  }
-  if (field === 'eyr') {
-    return 2020 <= +value && +value <= 2030;
-  }
-  if (field === 'hgt') {
-    const match = value.match(new RegExp(/^[\d]+(cm|in)$/));
-    if (!match) {
-      return false;
-    }
-    const unit = value.slice(value.length - 2);
-    const number = value.slice(0, value.length - 2);
-    if (unit === 'cm') {
-      return 150 <= +number && +number <= 193;
-    }
-    return 59 <= +number && +number <= 76;
-  }
-  if (field === 'hcl') {
-    return !!value.match(new RegExp(/^#[0-9a-f]{6}$/));
-  }
-  if (field === 'ecl') {
-    return ['amb', 'blu', 'brn', 'gry', 'grn', 'hzl', 'oth'].includes(value);
-  }
-  if (field === 'pid') {
-    return !!value.match(new RegExp(/^\d{9}$/));
-  }
-  return true;
 };
 
 const isValid = (passport: string) => {
