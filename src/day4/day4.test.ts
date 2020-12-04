34360cff4a3ed601baa9e94f30cf1fa64c0b1406
Benjamin Piggin <piggin.ben@gmail.com>
2020-12-04 21:01:52 +0000

    Sort out input

---
+++ src/day4/day4.test.ts
@@ -0,0 +1,14 @@
+import { day4 } from './day4';
+import { logAnswer } from '../utils/logging';
+import { data, testData } from './day4.data';
+
+test('Provided test cases', () => {
+  expect(day4(testData)).toBe(4);
+});
+
+test('Returns an answer', () => {
+  const answer = day4(data);
+  logAnswer(answer);
+  expect(typeof answer).toBe('number');
+  expect(answer).toBeGreaterThan(0);
+});
---
+++ src/day4/day4.ts
@@ -0,0 +1,4 @@
+export const day4 = (input: string[]) => {
+  console.log(input);
+  return 4;
+};
